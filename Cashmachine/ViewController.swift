12bcf3ce4a701b839c059ee2fb6723a8a9c52f7c
Igor.oh <ogayigore1996@mail.ru>
2020-01-06 13:44:36 +0300

    Initial Commit

---
+++ Cashmachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cashmachine
+//
+//  Created by Игорь Огай on 06.01.2020.
+//  Copyright © 2020 Игорь Огай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
